4b20e15ce60507295c73b6b0bf08847c7cec88af
MR. Subi <bantawasubin@gmail.com>
2024-10-07 23:52:34 +0545

    use of expo tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,30 @@
-import { Link, Slot } from "expo-router";
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
 
-export default function Layout(){
-    return <SafeAreaView>
-      <View style={{height:"90%"}}>
-      <Slot/>
-      </View>
-      <View style={{backgroundColor:"green"}}>
-        <Text>Hi lAYOUT</Text>
-        <Link href={"/"}>
-        <Text>Take me to the foryou page</Text>
-      </Link>
-      <Link href={"/explore"}>
-        <Text>Take me to the explore page</Text>
-      </Link>
-      <Link href={'/account'}>
-        <Text>Take me to the accounts page</Text>
-      </Link>
-      </View>
-    </SafeAreaView>
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'green' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'For you',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="compass" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
