d4a028c3e2dd16c64c58ab46cebc6385812214e4
MR. Subi <bantawasubin@gmail.com>
2024-10-08 22:30:08 +0545

    self added top bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,17 @@
+import { Link } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function foryou(){
     return <View>
         <Text>For You Page</Text>
+        <Link href={"/suggested"}>
+            <Text>Suggested</Text>
+        </Link>
+        <Link href={"/liked"}>
+            <Text>liked</Text>
+        </Link>
+        <Link href={"/library"}>
+            <Text>library</Text>
+        </Link>
     </View>
 }
