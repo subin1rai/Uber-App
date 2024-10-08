50dc0cbe10dc3be1535a837e3c7d45d613a13575
MR. Subi <bantawasubin@gmail.com>
2024-10-08 17:04:43 +0545

    grouping tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,38 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+import { SafeAreaView } from 'react-native';
+
+export default function TabLayout() {
+  return (
+
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: 'green',
+          headerShown: false,
+          tabBarStyle: { paddingBottom: 0, height: 50 },  // You can adjust height
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'For you',
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="account"
+          options={{
+            title: 'Account',
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: 'Explore',
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="compass" color={color} />,
+          }}
+        />
+      </Tabs>
+  );
+}
--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,7 +1,11 @@
+import { Link } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function explore(){
     return <View>
         <Text>Explore Page</Text>
+        <Link href={"/accountinfo"}>
+        <Text>Account Information</Text>
+        </Link>
     </View>
 }
