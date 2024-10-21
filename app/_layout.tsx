1c8efa752f11764632d9585d933545c63098477f
MR. Subi <bantawasubin@gmail.com>
2024-10-21 20:03:26 +0545

    stack changed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Layout() {
   return (
+    //main layout
     <GestureHandlerRootView>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen
