b5963f57b0aade0dd8a8f0ad025f92df190b8e89
MR. Subi <bantawasubin@gmail.com>
2024-10-30 23:34:00 +0545

    Image cards added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function Layout() {
   return (
     //main layout
-    //main layout
     <GestureHandlerRootView>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen
---
+++ hooks/useWallpaper.ts
@@ -0,0 +1,32 @@
+export interface Wallpaper {
+    url: string;
+    name: string;
+}
+
+export function useWallpaper(): Wallpaper[]{
+    return [
+        {
+        url:"https://ideogram.ai/assets/progressive-image/balanced/response/KN_Yf2xNTgmV2_PVoXYcIg",
+        name:"deer"
+    },
+        {
+        url:"https://ideogram.ai/assets/image/lossless/response/tge9rxvuR8WhjiL2_RLTnw",
+        name:"ghost"
+    },
+        {
+        url:"https://ideogram.ai/assets/progressive-image/balanced/response/kNCqpCt9RFGEGpffIUQ7Mg",
+        name:"flower"
+    },
+        {
+        url:"https://ideogram.ai/assets/progressive-image/balanced/response/GRUgqzx1SneOd_FP6wvr6g",
+        name:"flower2"
+    },
+        {
+        url:"https://ideogram.ai/assets/progressive-image/balanced/response/f13Vw_gkQkO1VWfswA6Ayw",
+        name:"deer2"
+    },
+]
+}
+
+
+
